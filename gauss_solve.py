86855eb2219ed39e958540cd035f411c3c75e68c
Ian Luff <141607310+iannluff@users.noreply.github.com>
2024-10-06 21:30:37 -0700

    1.0

--- gauss_solve.py
+++ gauss_solve.py
@@ -1,17 +1,11 @@
-#----------------------------------------------------------------
-# File:     gauss_solve.py
-#----------------------------------------------------------------
-#
-# Author:   Marek Rychlik (rychlik@arizona.edu)
-# Date:     Thu Sep 26 10:38:32 2024
-# Copying:  (C) Marek Rychlik, 2020. All rights reserved.
-# 
-#----------------------------------------------------------------
-# A Python wrapper module around the C library libgauss.so
-
 import ctypes
+import numpy as np
+#from scipy.linalg import hilbert
 
 gauss_library_path = './libgauss.so'
+
+class NoImplementationInC(Exception):
+    pass
 
 def unpack(A):
     """ Extract L and U parts from A, fill with 0's and 1's """
@@ -66,6 +60,62 @@ def lu_python(A):
 
     return unpack(A)
 
+# def plu_python(A):
+#     n = len(A)
+#     P = [[0 for _ in range(n)] for _ in range(n)]
+    
+#     # Place ones on the diagonal
+#     for i in range(n):
+#         P[i][i] = 1
+    
+#     for k in range(n-1):
+#         max_row_index = k
+#         max_value = abs(A[k][k])
+#         for l in range(k+1, n):
+#             if abs(A[l][k]) > max_value:
+#                 max_value = abs(A[l][k])
+#                 max_row_index = l    
+#         if max_row_index != k:
+#             A[k], A[max_row_index] = A[max_row_index], A[k]
+#             P[k], P[max_row_index] = P[max_row_index], P[k]
+#         for i in range(k,n):
+#             for j in range(k):
+#                 A[k][i] -= A[k][j] * A[j][i]
+#         for i in range(k+1, n):
+#             for j in range(k):
+#                 A[i][k] -= A[i][j] * A[j][k]
+#             A[i][k] /= A[k][k]
+    
+#     L, U = unpack(A)
+    
+#     return P, L, U
+
+def plu_python(A):
+    n = len(A)
+    P = np.eye(n)
+    U = np.array(A, dtype=float)
+    L = np.zeros((n,n), dtype=float)
+    
+    for k in range(n-1):
+        max_row_index = k
+        max_value = abs(U[k][k])
+        for l in range(k+1, n):
+            if abs(U[l][k]) > max_value:
+                max_value = abs(U[l][k])
+                max_row_index = l    
+        if max_row_index != k:
+            U[[k, max_row_index]] = U[[max_row_index, k]]
+            P[[k, max_row_index]] = P[[max_row_index, k]]
+            L[[k, max_row_index], 0:k] = L[[max_row_index, k], 0:k] 
+        for i in range(k+1, n):
+            L[i][k] = U[i][k] / U[k][k]
+            for j in range(k, n):
+                U[i][j] -= L[i][k]*U[k][j]
+    
+    for m in range(n):
+        L[m][m] = 1
+    
+    return P, L, U
 
 def lu(A, use_c=False):
     if use_c:
@@ -73,26 +123,26 @@ def lu(A, use_c=False):
     else:
         return lu_python(A)
 
-
+def plu(A, use_c=False):
+    if use_c:
+        raise NoImplementationInC()
+    else:
+        return plu_python(A)
 
 if __name__ == "__main__":
 
     def get_A():
         """ Make a test matrix """
-        A = [[2.0, 3.0, -1.0],
-             [4.0, 1.0, 2.0],
-             [-2.0, 7.0, 2.0]]
+        A = [[4.0, 9.0, 10.0],
+              [14.0, 30.0, 34.0],
+              [2.0, 3.0, 3.0]]
         return A
 
     A = get_A()
 
-    L, U = lu(A, use_c = False)
-    print(L)
-    print(U)
-
-    # Must re-initialize A as it was destroyed
+    P, L, U = plu(A, use_c = False)
+    
     A = get_A()
 
-    L, U = lu(A, use_c=True)
-    print(L)
-    print(U)
+    P, L, U = plu(A, use_c=True)
+    
